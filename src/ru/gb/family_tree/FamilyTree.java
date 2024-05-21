b1e5714ba798644b110f70e7a0f9930593130fee
SlanSaf <slansaf@yandex.ru>
2024-05-21 15:44:01 +0300

    Добавлены все методы в Human и FamilyTree как у преподавателя

--- src/ru/gb/family_tree/FamilyTree.java
+++ src/ru/gb/family_tree/FamilyTree.java
@@ -4,19 +4,114 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FamilyTree {
+    private  long humansId;
     private List<Human> humanList;
 
-    public FamilyTree() {
-        humanList = new ArrayList<>();
+    public FamilyTree(){
+        this(new ArrayList<>());
     }
 
-    public void addHuman(Human human){
-        humanList.add(human);
+    public FamilyTree(List<Human> humanList) {
+        this.humanList = humanList;
     }
 
-    public Human findByName(String nameHuman){
-        for(Human human: humanList){
-            if(human.getName().equalsIgnoreCase(nameHuman)){
+    public boolean addHuman(Human human){
+        if (human == null){
+            return false;
+        }
+        if (!humanList.contains(human)){
+            humanList.add(human);
+            human.setId(humansId++);
+
+            addToParents(human);
+            addToChildern(human);
+
+            return true;
+        }
+        return false;
+    }
+
+    private void addToParents(Human human){
+        for (Human parent: human.getParents()){
+            parent.addChild(human);
+        }
+    }
+
+    private void addToChildern(Human human){
+        for (Human child: human.getChildren()){
+            child.addParent(human);
+        }
+    }
+
+    public List<Human> getSiblings(int id){
+        Human human = getById(id);
+        if (human == null){
+            return  null;
+        }
+        List<Human> res = new ArrayList<>();
+        for (Human parent: human.getParents()){
+            for(Human child: parent.getChildren()){
+                if(!child.equals(human)){
+                    res.add(child);
+                }
+            }
+        }
+        return res;
+    }
+
+    public List<Human> findByName(String name){
+        List<Human> res = new ArrayList<>();
+        for (Human human: humanList){
+            if (human.getName().equals(name)){
+                res.add(human);
+            }
+        }
+        return res;
+    }
+
+    public boolean setWedding(long humansId1, long humansId2){
+        if(checkId(humansId1) && checkId(humansId2)){
+            Human human1 = getById(humansId1);
+            Human human2 = getById(humansId2);
+            if (human1.getSpouse() == null && human2.getSpouse() == null){
+                human1.setSpouse(human2);
+                human2.setSpouse(human1);
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public boolean setDivorse(long humansId1, long humansId2){
+        if(checkId(humansId1) && checkId(humansId2)){
+            Human human1 = getById(humansId1);
+            Human human2 = getById(humansId2);
+            if (human1.getSpouse() != null && human2.getSpouse() != null){
+                human1.setSpouse(null);
+                human2.setSpouse(null);
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public boolean remuve(long humansId){
+        if(checkId(humansId)){
+            Human human = getById(humansId);
+            return  humanList.remove(human);
+        }
+        return false;
+    }
+
+    private boolean checkId(long id){
+        return id < humansId && id >= 0;
+    }
+
+    public  Human getById(long id){
+        for (Human human: humanList){
+            if(human.getId() == id){
                 return human;
             }
         }
@@ -24,9 +119,15 @@ public class FamilyTree {
     }
 
     @Override
-    public String toString() {
+    public String toString(){
+        return getInfo();
+    }
+
+    public String getInfo() {
        StringBuilder stringBuilder = new StringBuilder();
-       stringBuilder.append("Список людей:\n");
+       stringBuilder.append("В дереве ");
+       stringBuilder.append(humanList.size());
+       stringBuilder.append("объектов: \n");
         for (Human human : humanList) {
             stringBuilder.append(human);
             stringBuilder.append("\n");
